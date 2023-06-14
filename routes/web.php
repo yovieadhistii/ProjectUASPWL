d9adf818fe62bd4affd5ca00de09a67170978371
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-06-14 21:21:24 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 use \App\Http\Controllers\MahasiswaController;
+use \App\Http\Controllers\DashboardProdiController;
+use App\Http\Controllers\MataKuliahController;
 
 Route::get('/', function() {
     return redirect(route('login'));
@@ -24,5 +26,12 @@ Auth::routes(['verify' => false, 'reset' => false]);
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
     Route::get('/mahasiswa',[MahasiswaController::class,'index'])->name('dashboardmahasiswa');
+    Route::get('/perwalian',[MataKuliahController::class,'index'])->name('perwalian');
+    Route::get('/Prodi/dashboardProdi', [DashboardProdiController::class,'index'])->name('dashboardProdi');
+    Route::get('/Prodi/create',[DashboardProdiController::class,'create'])->name('createProdi');
+    Route::post('/Prodi/create',[DashboardProdiController::class,'store'])->name('storeProdi'); 
+    Route::get('/matkul/dashboard', [MataKuliahController::class,'index'])->name('lihatMK');
+    Route::get('/matkul/create', [MataKuliahController::class,'create'])->name('createMK');
+    Route::post('/matkul/create',[MataKuliahController::class,'store'])->name('storeMK'); 
 });
 
