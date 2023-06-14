09757664aaa39ca87e0a94bf08e5360b9765b125
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-06-14 21:23:12 +0700

    Add files via upload

---
+++ app/Http/Controllers/MataKuliahController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MataKuliah;
+use Illuminate\Http\Request;
+
+class MataKuliahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = MataKuliah::all();
+        return view('matkul.index',[
+            'matkuls'=> $data
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('/matkul/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = validator($request->all(),[
+            'kdMK' => 'required|string|max:100',
+            'txtName' => 'required|string|max:100',
+            'sks' => 'required|integer',
+            'semester' => 'required|integer'
+        ])->validate();
+        $mk = new MataKuliah();
+        $mk -> kode = $validatedData['kdMK'];
+        $mk -> nama = $validatedData['txtName'];
+        $mk -> sks = $validatedData['sks'];
+        $mk -> semester = $validatedData['semester'];
+        $mk -> program_studi_kode_prodi = 72;
+        $mk -> save(); 
+        return  redirect(route('lihatMK'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MataKuliah  $mataKuliah
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MataKuliah $mataKuliah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MataKuliah  $mataKuliah
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MataKuliah $mataKuliah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MataKuliah  $mataKuliah
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MataKuliah $mataKuliah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MataKuliah  $mataKuliah
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MataKuliah $mataKuliah)
+    {
+        //
+    }
+}
